c08acccb4b9543ef1697cf38a80044ead1d945ca
florist476 <161070925+florist476@users.noreply.github.com>
2025-07-30 20:55:49 +0300

    end

--- one.py
+++ one.py
@@ -11,10 +11,32 @@ def news():
 
 @app.route("/gadgets")
 def gadgets():
-    return render_template("gadgets.html")
+    return render_template("gad.html")
 
 @app.route("/ai")
 def ai():
     return render_template("ai.html")
+
+@app.route("/smartphone")
+def smartph():
+    return render_template("smartph.html")
+
+@app.route("/innovtion")
+def innovation():
+    return render_template("innovtion.html")
+
+@app.route("/laptops")
+def lapptops():
+    return render_template("laptops.html")
+
+@app.route("/smartwatch")
+def smarywht():
+    return render_template("smartwatch.html")
+
+@app.route("/cybersecurity")
+def cybersrc():
+    return render_template("cybersecurity.html")
+
+
 
 app.run(debug=True)
